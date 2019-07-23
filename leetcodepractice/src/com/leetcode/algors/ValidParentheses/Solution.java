28aa018e5f4970f1cc358229c2a7aa996f546bca
AAP <AAP@10.0.0.198>
2019-07-22 20:57:53 -0700

    com.leetcode.algors.ValidParentheses - initial changes

---
+++ leetcodepractice/src/com/leetcode/algors/ValidParentheses/Solution.java
@@ -0,0 +1,9 @@
+package com.leetcode.algors.ValidParentheses;
+
+class Solution {
+    public boolean isValid(String s) {
+        
+    	
+    	return false;
+    }
+}
---
+++ leetcodepractice/src/com/leetcode/algors/ValidParentheses/Start.java
@@ -0,0 +1,14 @@
+package com.leetcode.algors.ValidParentheses;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "{}[]()";
+		
+		Solution sl = new Solution();
+		boolean result = sl.isValid(str);
+		System.out.println(result);
+	}
+
+}
