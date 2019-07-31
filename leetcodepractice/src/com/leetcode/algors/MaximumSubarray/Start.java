f55869cef265c0baed8bb372b2b70f3243a9b940
AAP <AAP@10.0.0.198>
2019-07-30 22:49:22 -0700

    com.leetcode.algors.MaximumSubarray - Solved, Submitted, Tested, Accepted

---
+++ leetcodepractice/src/com/leetcode/algors/MaximumSubarray/Start.java
@@ -0,0 +1,15 @@
+package com.leetcode.algors.MaximumSubarray;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Solution sl = new Solution();
+		int[] arr = {-1};
+		int result = sl.maxSubArray(arr);
+		
+		System.out.println(result);
+	}
+}
