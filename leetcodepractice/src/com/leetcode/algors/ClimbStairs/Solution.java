50764a2f45c96aece508c73f53b408e2da5e352f
AAP <AAP@10.0.0.198>
2019-08-08 21:38:51 -0700

    com.leetcode.algors.ClimbStair - initial changes

---
+++ leetcodepractice/src/com/leetcode/algors/ClimbStairs/Solution.java
@@ -0,0 +1,10 @@
+package com.leetcode.algors.ClimbStairs;
+// https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+    public int climbStairs(int n) {
+        
+    	
+    	return 0;
+    }
+}
---
+++ leetcodepractice/src/com/leetcode/algors/ClimbStairs/Start.java
@@ -0,0 +1,22 @@
+package com.leetcode.algors.ClimbStairs;
+// https://leetcode.com/problems/climbing-stairs/
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Solution sl = new Solution();
+		int result = sl.climbStairs(8);
+		System.out.println(result);
+	}
+}
+
+/* 
+ * 000001
+ *   1111
+ * 010000
+ * 
+ * 
+ * */
