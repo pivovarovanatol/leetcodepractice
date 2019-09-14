4dc5610c4ea94963fec8528d2c038c0c6477358d
AAP <AAP@10.0.0.198>
2019-09-13 22:15:11 -0700

    com.leetcode.algors.BalancedBinaryTree - Solved, Submitted, Tested, Accepted

---
+++ leetcodepractice/src/com/leetcode/algors/BalancedBinaryTree/Solution.java
@@ -0,0 +1,59 @@
+package com.leetcode.algors.BalancedBinaryTree;
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        
+    	return isBalancedHelper(root);
+    }
+    
+    boolean isBalancedHelper(TreeNode root) {
+    	
+    	if (root==null) {
+    		return true;
+    	}
+    	
+    	int height = getHeight(root);
+    	
+    	//System.out.println("Left height: " + heightLeft + ", Right height: " + heightRight);
+    	if (height==-1) {
+    		return false;
+    	} 
+    	return true;
+
+    }
+
+    int getHeight(TreeNode root) {
+    	if (root == null) {
+    		return 0;
+    	}
+
+    	int left = 0;
+    	int right = 0;
+    	if (root.left!=null) {
+    		left = getHeight(root.left);
+    	}
+    	
+    	if (left == -1) {
+    		return -1;
+    	}
+    	
+    	if (root.right!=null) {
+    		right = getHeight(root.right);
+    	}
+    	
+    	if (right==-1) {
+    		return -1;
+    	}
+    	
+    	int diff = Math.abs(left-right);
+    	
+    	if (diff > 1) {
+    		return -1;
+    	}
+    	
+    	return (1 + (left>right ? left : right));
+    }
+    
+}
