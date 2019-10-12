6a94754ae909279a81f4a4af49e2f46df7201fac
AAP <AAP@10.0.0.198>
2019-10-11 21:41:25 -0700

    com.leetcode.algors.SingleNumber - solved with HashMap - to slow in my opinion. Need to try something different.

--- leetcodepractice/src/com/leetcode/algors/SingleNumber/Solution.java
+++ leetcodepractice/src/com/leetcode/algors/SingleNumber/Solution.java
@@ -5,17 +5,27 @@ import java.util.HashMap;
 class Solution {
     public int singleNumber(int[] nums) {
         
-    	HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
     	int n = nums.length;
     	// traverse thru array and add it to a hashmap
     	
+    	HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+    	
     	for (int i=0;i<n;i++) {
     		if (hm.containsKey(nums[i])) {
-    			hm.
+    			int tmp = hm.get(nums[i]);
+    			hm.put(nums[i], tmp + 1);
+    		} else {
+        		hm.put(nums[i], 1);
+    		}
+    	}
+
+    	for(int i=0;i<n;i++) {
+    		if (hm.get(nums[i]) == 1) {
+    			return nums[i];
     		}
     	}
     	
-    	
     	return 0;
     }
+    
 }
