60971d6387809a2a287d2ae290614fdfeb4b2f8a
I855719 <I855719@VANN34702302A.amer.global.corp.sap>
2019-10-28 14:15:34 -0700

    com.leetcode.algors.MajorityElement - solved by sorting the input array and then traversing with noting the count for the same element.

--- leetcodepractice/src/com/leetcode/algors/MajorityElement/Solution.java
+++ leetcodepractice/src/com/leetcode/algors/MajorityElement/Solution.java
@@ -1,12 +1,13 @@
 package com.leetcode.algors.MajorityElement;
 
+import java.util.Arrays;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 // https://leetcode.com/problems/majority-element/
 
 class Solution {
-    public int majorityElement(int[] nums) {
+    public int majorityElement2(int[] nums) {
         
     	int n = nums.length;
         int result = 0;
@@ -30,4 +31,26 @@ class Solution {
         
         return result;
     }
+
+
+    public int majorityElement(int[] nums) {
+    	
+    	Arrays.sort(nums);
+    	
+        if(nums == null || nums.length == 0) return 0;
+        int preVal = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == preVal) count++;
+            else {
+                count--;
+                if(count == 0) {
+                    preVal = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return preVal;
+    }
+
 }
--- leetcodepractice/src/com/leetcode/algors/MajorityElement/Start.java
+++ leetcodepractice/src/com/leetcode/algors/MajorityElement/Start.java
@@ -6,7 +6,7 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] arr1 = {3,3,4};
+		int[] arr1 = {4,1,4,2,4,5,6,4,7};
 
 		Solution sl = new Solution();
 		int result = sl.majorityElement(arr1);
