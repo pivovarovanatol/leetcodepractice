72b6bb6e528f30ed4088dd1550b9890e63a7160a
Anatoliy Pivovarov <pivovarovanatol@gmail.com>
2019-11-13 14:15:05 -0800

    com.leetcode.algors.FirstBadVersion - solved with recursion and iteratively. 10ms(<99%) 33MB(<100%)

---
+++ leetcodepractice/src/com/leetcode/algors/FirstBadVersion/Start.java
@@ -0,0 +1,17 @@
+package com.leetcode.algors.FirstBadVersion;
+// https://leetcode.com/problems/first-bad-version/
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		Solution sl = new Solution();
+		int result = sl.firstBadVersion(2929);
+
+		System.out.println(result);
+	}
+    
+}
+
