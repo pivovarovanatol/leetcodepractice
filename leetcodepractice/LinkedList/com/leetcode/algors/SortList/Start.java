7c520162d91c35d4d5edff798f44ea657f44a31e
I855719 <I855719@VANN34702302A.amer.global.corp.sap>
2019-11-18 15:32:10 -0800

    com.leetcode.algors.SortList - solved with Merge Sort form Discussions. O(nlogn). 4 ms (<64%) 40MB(<89%). Need to review later, especially part of getting the middle and merging.

--- leetcodepractice/LinkedList/com/leetcode/algors/SortList/Start.java
+++ leetcodepractice/LinkedList/com/leetcode/algors/SortList/Start.java
@@ -1,8 +1,9 @@
 package com.leetcode.algors.SortList;
+//https://leetcode.com/problems/sort-list/
 
-import java.io.BufferedReader;
+// import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+// import java.io.InputStreamReader;
 
 public class Start {
     public static int[] stringToIntegerArray(String input) {
