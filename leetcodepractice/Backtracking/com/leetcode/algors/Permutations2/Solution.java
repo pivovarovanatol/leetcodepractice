74a4573dee99f1d75c6767beb7239adda1b3db9f
Anatoliy Pivovarov <pivovarovanatol@gmail.com>
2019-11-21 16:41:21 -0800

    com.leetcode.algors.Permutations2 - solved with Set + recursion. Not cool. 68ms(<12%) 41MB(<13%)

--- leetcodepractice/Backtracking/com/leetcode/algors/Permutations2/Solution.java
+++ leetcodepractice/Backtracking/com/leetcode/algors/Permutations2/Solution.java
@@ -2,9 +2,12 @@ package com.leetcode.algors.Permutations2;
 // https://leetcode.com/problems/permutations-ii/
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Solution {
+	Set<String> set = new HashSet<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
         
         List<List<Integer>> result = new ArrayList<>();
@@ -17,14 +20,25 @@ class Solution {
     
     void permute(int start, int end, int[] nums, List<List<Integer>> result){
         
-        if (start == end){
-            printArray(nums);            
+        if (start >= end){
+            // printArray(nums);
+        	List<Integer> list = new ArrayList<>();
+        	String tmp = "";
+            for (int j=0;j<=end;j++) {
+            	list.add(nums[j]);
+            	tmp = tmp + "|" + nums[j]; 
+            }
+            if (!set.contains(tmp)) {
+            	set.add(tmp);
+                result.add(list);
+            }
+            return;
         }
         
         
         for (int i=start;i<=end;i++){
             swap(start, i, nums);
-            permute(i+1, end, nums,result);
+            permute(start+1, end, nums,result);
             swap(start, i, nums);
         }
         
