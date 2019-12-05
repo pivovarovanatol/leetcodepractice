2b208459df9c132dcfe845faaa1a3ff45c34391c
Anatoliy Pivovarov <pivovarovanatol@gmail.com>
2019-12-04 22:26:20 -0800

    com.leetcode.algors.FairCandySwap - trying to improve. Have some issues.

--- leetcodepractice/Array/com/leetcode/algors/FairCandySwap/FairCandySwapTest.java
+++ leetcodepractice/Array/com/leetcode/algors/FairCandySwap/FairCandySwapTest.java
@@ -10,19 +10,136 @@ public class FairCandySwapTest {
 		// given
 		int[] arr1 = {2};
 		int[] arr2 = {1,3};
-
 		Solution sl = new Solution();
 
 		// when
 		int[] result = sl.fairCandySwap(arr1, arr2);		
-		
+
 		// then
 		int[] expected = {2,3};
-		
 		Assert.assertArrayEquals(expected, result);
-		
 		//System.out.println(result[0] + " " + result[1]);
 		//System.out.print(" ");
-		
 	}
+
+	@Test
+	public void FairCandySwapCase2(){
+		// given
+		int[] arr1 = {1,1};
+		int[] arr2 = {2,2};
+		Solution sl = new Solution();
+
+		// when
+		int[] result = sl.fairCandySwap(arr1, arr2);		
+
+		// then
+		int[] expected = {1,2};
+		Assert.assertArrayEquals(expected, result);
+		//System.out.println(result[0] + " " + result[1]);
+		//System.out.print(" ");
+	}
+
+	@Test
+	public void FairCandySwapCase3(){
+		// given
+		int[] arr1 = {1,2};
+		int[] arr2 = {2,3};
+		Solution sl = new Solution();
+
+		// when
+		int[] result = sl.fairCandySwap(arr1, arr2);		
+
+		// then
+		int[] expected = {1,2};
+		Assert.assertArrayEquals(expected, result);
+		//System.out.println(result[0] + " " + result[1]);
+		//System.out.print(" ");
+	}
+
+	@Test
+	public void FairCandySwapCase4(){
+		// given
+		int[] arr1 = {1,2,5};
+		int[] arr2 = {2,4};
+		Solution sl = new Solution();
+
+		// when
+		int[] result = sl.fairCandySwap(arr1, arr2);		
+
+		// then
+		int[] expected = {5,4};
+		Assert.assertArrayEquals(expected, result);
+		//System.out.println(result[0] + " " + result[1]);
+		//System.out.print(" ");
+	}
+
+	@Test
+	public void FairCandySwapCase5(){
+		// given
+		int[] arr1 = {35,17,4,24,10};
+		int[] arr2 = {63,21};
+		Solution sl = new Solution();
+
+		// when
+		int[] result = sl.fairCandySwap(arr1, arr2);		
+
+		// then
+		int[] expected = {24,21};
+		Assert.assertArrayEquals(expected, result);
+		//System.out.println(result[0] + " " + result[1]);
+		//System.out.print(" ");
+	}
+	
+	@Test
+	public void FairCandySwapCase6(){
+		// given
+		int[] arr1 = {1,17,14,1,16};
+		int[] arr2 = {26,11};
+		Solution sl = new Solution();
+
+		// when
+		int[] result = sl.fairCandySwap(arr1, arr2);		
+
+		// then
+		int[] expected = {17,11};
+		Assert.assertArrayEquals(expected, result);
+		//System.out.println(result[0] + " " + result[1]);
+		//System.out.print(" ");
+	}
+		
+	@Test
+	public void FairCandySwapCase7(){
+		// given
+		int[] arr1 = {8,73,2,86,32};
+		int[] arr2 = {56,5,67,100,31};
+		Solution sl = new Solution();
+
+		// when
+		int[] result = sl.fairCandySwap(arr1, arr2);		
+
+		// then
+		int[] expected = {2,31};
+		Assert.assertArrayEquals(expected, result);
+		//System.out.println(result[0] + " " + result[1]);
+		//System.out.print(" ");
+	}
+			
+	@Test
+	public void FairCandySwapCase8(){
+		// given
+		int[] arr1 = {32,39,8,41,9,2,24,33,32,25,10,29,24,1,14,20,37,49,49,29};
+		int[] arr2 = {36,86,80,97,26,100,60,65,24,93};
+		Solution sl = new Solution();
+
+		// when
+		int[] result = sl.fairCandySwap(arr1, arr2);		
+
+		// then
+		int[] expected = {20,100};
+		Assert.assertArrayEquals(expected, result);
+		//System.out.println(result[0] + " " + result[1]);
+		//System.out.print(" ");
+	}
+			
+	
 }
--- leetcodepractice/Array/com/leetcode/algors/FairCandySwap/Solution.java
+++ leetcodepractice/Array/com/leetcode/algors/FairCandySwap/Solution.java
@@ -8,8 +8,66 @@ import java.util.TreeMap;
 
 class Solution {
 	
-	
     public int[] fairCandySwap(int[] A, int[] B) {
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int[] res = new int[2];
+        int sumB = 0;
+        int sumA = 0;
+        
+        for (int i=0;i<A.length;i++){
+            sumA+=A[i];    
+        }
+        
+        for (int i=0;i<B.length;i++){
+            sumB+=B[i];    
+        }
+        
+        int pointerA = 0;
+        int pointerB = 0;
+        int diff = (sumA - sumB)/2;
+        
+//        if (sumA > sumB) {
+//        	diff = diff * -1;
+//        }
+        
+        while (pointerA < A.length && pointerB < B.length) {
+        	int valA = A[pointerA];
+        	int valB = B[pointerB];
+        	if (valA - valB == diff) {
+        		res[0]=A[pointerA];
+        		res[1]=B[pointerB];
+        		return res;
+        	} 
+        	
+        	if (sumA > sumB) {
+        		if (A[pointerA] < B[pointerB]) {
+        			pointerA++;
+        			pointerB=0;
+        		} else {
+        			pointerB++;
+        		}
+        	} else {
+        		if (B[pointerB] < A[pointerA]) {
+        			pointerB++;
+        			pointerA=0;
+        		} else {
+        			pointerA++;
+        		}
+        	}
+        	
+        }
+        
+        return res;
+    }
+
+	
+	
+	
+	
+	
+    public int[] fairCandySwap2(int[] A, int[] B) {
         
         Arrays.sort(A);
         Arrays.sort(B);
