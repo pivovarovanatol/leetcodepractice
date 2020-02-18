2fc04b76d0f3f6e18fc55adf266a66fdc21b93cf
Anatoliy Pivovarov <pivovarovanatol@gmail.com>
2020-02-18 14:05:19 -0800

    com.leetcode.algors.CombinationSum - from discussions. 2 ms(<99%)

--- leetcodepractice/Backtracking/com/leetcode/algors/CombinationSum/Solution.java
+++ leetcodepractice/Backtracking/com/leetcode/algors/CombinationSum/Solution.java
@@ -8,8 +8,48 @@ import java.util.List;
 import java.util.Set;
 
 class Solution {
-	// solved with recursion and gradually reducing the target.  22 ms (<7%) 42MB(<5%)
+	
+	// from discussions. 2 ms(<99%). 
+    ArrayList<List<Integer>> r;
+    int[] values;
+    int[] counts;
+    
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        r = new ArrayList();
+        values = candidates;
+        counts = new int[values.length];
+        helper(0, target);
+        return r;
+    }
+	
+    void helper(int p, int n) {
+        if (p >= values.length) {
+            if (n == 0) {
+                ArrayList<Integer> x = new ArrayList();
+                r.add(x);
+                for (int i=0; i<values.length; i++) {
+                    Integer v = values[i];
+                    for (int j=0; j<counts[i]; j++) {
+                        x.add(v);
+                    }
+                }
+            }
+            return;
+        }
+        for (int c=0; ; c++) {
+            int s = c * values[p];
+            if (s > n) {
+                break;
+            }
+            counts[p] = c;
+            helper(p+1, n-s);
+        }
+    }
+	
+	
+	// solved with recursion and gradually reducing the target.  22 ms (<7%) 42MB(<5%)
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         // brute force with recursion/backtracking
         // get the candidate. then deduct candidate from target and repeat. once reached 0 -> add to the possible combination list.
         Set<List<Integer>> results = new HashSet<>();
