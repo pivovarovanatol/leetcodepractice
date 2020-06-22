2b14570f36f8362cca02a0a020990aaf5b8b574b
Anatoliy Pivovarov <pivovarovanatol@gmail.com>
2020-06-22 10:02:43 -0700

    com.leetcode.algors.SingleNumberII
    Solution is from discussions with bit=wise operations. Wasn't sure how
    to perform that, there is a set of operations to maintain
    ones/twos/threes

---
+++ leetcodepractice/BitManipulation/com/leetcode/algors/SingleNumberII/Start.java
@@ -0,0 +1,16 @@
+package com.leetcode.algors.SingleNumberII;
+// https://leetcode.com/problems/missing-number/
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] nums = {1,2,3,2,1};
+		
+		Solution sl = new Solution();
+		int result = sl.singleNumber(nums);
+		System.out.println(result);
+	}
+}
+
